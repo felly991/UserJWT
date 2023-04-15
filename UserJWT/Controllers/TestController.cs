968ffe6081a5a67c1bd02d8988b688ac68aed1dc
dev_danil <tinder12347@mail.ru>
2023-04-15 20:04:16 +0300

    Добавьте файлы проекта.

---
+++ UserJWT/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserJWT.UserData;
+
+namespace UserJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("test"), Authorize(Roles = "Admin")]
+        public ActionResult<User> Login2()
+        {
+            return Ok("test");
+        }
+    }
+}
